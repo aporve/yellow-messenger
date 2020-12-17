ca0ce35c676a3217d936f04a7ed38fcd0b04736d
aiswaryac895 <aiswarya.c@10xds.com>
2020-12-17 08:29:47 +0530

    intgrtns

--- assets/js/illness.js
+++ assets/js/illness.js
@@ -1717,6 +1717,13 @@ function handleAccountInfo(event) {
             }
             document.getElementById("submit9").disabled = true;
             document.getElementById("submit9").style.cursor = "no-drop";
+            var nodes = document.getElementById("bank_form").getElementsByTagName('*');
+            for (var i = 0; i < nodes.length; i++) {
+                nodes[i].disabled = true;
+                nodes[i].style.cursor = 'no-drop'
+
+            }
+            document.getElementById("bank_form").style.opacity = '0.65'
             finalSubmitCall();
             // myDisable()
             // timer().then(async () => {
@@ -1871,6 +1878,13 @@ function handleAddBankInfo(event) {
             BankDetailsList.push(BankDetails);
         document.getElementById("account_details1_btn").disabled = true;
         document.getElementById("account_details1_btn").style.cursor = "no-drop";
+        var nodes = document.getElementById("addbank_form").getElementsByTagName('*');
+        for (var i = 0; i < nodes.length; i++) {
+            nodes[i].disabled = true;
+            nodes[i].style.cursor = 'no-drop'
+
+        }
+        document.getElementById("addbank_form").style.opacity = '0.65'
         finalSubmitCall();
 
         // $("#step3_circle").addClass("md-step-step3-circle ");
