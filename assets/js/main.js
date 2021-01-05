6ed3f9744cfb44935442014b4e198821a2e8a0d2
Aporve <aporvep@gmail.com>
2021-01-05 13:46:35 +0530

    Payment box color changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -765,7 +765,7 @@ function setDeathClaimStatusMsg() {
                 else {
                     document.getElementById('claim-msg-text').innerHTML = '<div> <h3>YOUR PAYOUT HAS BEEN APPROVED</h3> <br /> <p class="font-weight-justy request-font"> We would like to let you know that we have approved your claim request for your AIA Philam Life policy . </p> <br /> <p class="font-weight-normal request-font"> Kindly expect to receive the benefit through your chosen payout method. Please expect an update from us on when your benefit will be released. </p> <br /> <p class="font-weight-normal request-font"> We have also sent this information via SMS for your reference. </p> </div>'
                     document.getElementById("turnaround-time-ref").style.display = "none";
-                    document.getElementById("payment-ref").style.display = "none";
+                    document.getElementById("payment-ref").style.display = "block";
                 }
 
                 allStepperActive()
