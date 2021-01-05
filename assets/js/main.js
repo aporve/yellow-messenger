a7f47338ebcf562b8a8410d6844c080091bdd070
Aporve <aporvep@gmail.com>
2021-01-05 15:16:25 +0530

    Payment box color changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,7 +242,7 @@ function trackProgress() {
                             if (claimAmount != null && claimAmount != '' && claimAmount != '0.0' && claimAmount != '0.00') { document.getElementById('payment_amount').innerHTML = currency + ' ' + claimAmount; }
                             else {
                                 document.getElementById('payment_amount').innerHTML = ''
-                                $("#payment-ref").css("background-color", "gray");
+                                $("#payment-ref").css("background-color", "lightgray");
                             }
 
                             displayDateForClaimStatus()
